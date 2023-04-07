d6820e5e6f40deb5a95398c28ffd15a60bd9f8da
chusmithder <chusmithder@gmail.com>
2023-04-07 21:55:47 +0200

    agragada logica primera

---
+++ revdiet/lib/screens/1_signin_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({super.key});
+  
+  @override
+  State<StatefulWidget> createState() {
+    return SignInScreenState();
+  }
+}
+
+class SignInScreenState extends State<StatefulWidget> {
+  @override
+  Widget build(BuildContext context) {
+    final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+    return Scaffold(
+      
+    );
+  }
+
+}
---
+++ revdiet/lib/screens/2_signup_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({super.key});
+  
+  @override
+  State<StatefulWidget> createState() {
+    return SignUpScreenState();
+  }
+}
+
+class SignUpScreenState extends State<StatefulWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        
+      ),
+    );
+  }
+
+}
